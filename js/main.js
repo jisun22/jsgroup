cd14ae4d03cb7cea019e27ac08be37fd01d03c2d
jisun22 <hayanangela@gmail.com>
2021-11-09 19:01:40 +0900

    made

--- js/main.js
+++ js/main.js
@@ -1,29 +1,22 @@
 // 헤더 색상 변하기
 
 $(function(){
-    var $header = $('#header'); //헤더를 변수에 넣기
-    var $page = $('.companies-text'); //색상이 변할 부분
-    var $window = $(window);
-    var pageOffsetTop = $page.offset().top;//색상 변할 부분의 top값 구하기
-    
-    $window.resize(function(){ //반응형을 대비하여 리사이즈시 top값을 다시 계산
-      pageOffsetTop = $page.offset().top;
-    });
-    
-    $window.on('scroll', function(){ //스크롤시
-      var scrolled = $window.scrollTop() >= pageOffsetTop; //스크롤된 상태; true or false
-      $header.toggleClass('down', scrolled); //클래스 토글
-    });
+  var $header = $('.header-nav'); //헤더를 변수에 넣기
+  var $page = $('.companies-text'); //색상이 변할 부분
+  var $window = $(window);
+  var pageOffsetTop = $page.offset().top;//색상 변할 부분의 top값 구하기
+  
+  $window.resize(function(){ //반응형을 대비하여 리사이즈시 top값을 다시 계산
+    pageOffsetTop = $page.offset().top;
   });
+  
+  $window.on('scroll', function(){ //스크롤시
+    var scrolled = $window.scrollTop() >= pageOffsetTop; //스크롤된 상태; true or false
+    $header.toggleClass('down', scrolled); //클래스 토글
+  });
+});
 
 // 헤더 서브메뉴 드롭다운
-// $("#num1").hover(function(){
-//     $(this).children('sub').stop().slideToggle(500);
-// });
-
-// $("#num2").hover(function(){
-//   $(this).children('sub').stop().slideToggle(500);
-// });
 
 $(document).ready(function(){
   $("#num>li").mouseover(function(){
@@ -49,11 +42,22 @@ var swiperHeader = new Swiper('.swiper-head', {
     speed: 3000,
     effect : 'fade'
 });
+// 헤더 슬라이드 모바일
+var swiperHeader = new Swiper('.swiper-head-mobile', {
+  mousewheel: false,
+  keyboard: false,
+  slidesPerView: 1,
+  autoplay: {
+      delay: 2000,
+  },
+  speed: 3000,
+  effect : 'fade'
+});
 
 var swiperContent = new Swiper('.swiper-content', {
-    slidesPerView: 4,
-    spaceBetween: 10,
-    centeredSlides: true,
+    slidesPerView: 3,
+    spaceBetween: 50,
+    // centeredSlides: true,
     // pagination: {
     //   el: '.swiper-pagination',
     //   clickable: true,
@@ -91,4 +95,10 @@ $('#test_next').click(function(){
 
 
 
+const toggleBtn = document.querySelector('.toggleBtn');
+const menu = document.querySelector('.header-menu li');
 
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    links.classList.toggle('active');
+})
